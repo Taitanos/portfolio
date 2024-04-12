2c0274a2a61444c74c91193e450e81dea76fb614
Taitanos <hayaashi87@gmail.com>
2024-04-12 22:37:29 +0300

    Created global styled and theme

---
+++ src/styled/Theme.ts
@@ -0,0 +1,14 @@
+
+export const Theme = {
+    color: {
+        primaryBg: 'linear-gradient(90deg, rgba(79, 71, 200, 0.4), rgba(29, 213, 208, 0.22)',
+        secondaryBg: '#343D68',
+        HeaderBg: '#FCFCFC',
+
+        fontTitle: '#E84949',
+        fontDescription: '#818080',
+        fontSkills: '#000',
+        fontText: '#fff',
+        fontGit: '#0ACBBF',
+    }
+}
